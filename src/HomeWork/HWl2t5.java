c3bd4a6710bb5a0c9347ad8ac530b1ee04b5a385
axepoh616 <57939572+axepoh616@users.noreply.github.com>
2019-11-24 14:15:13 +0300

    Create HWl2t5.java

---
+++ src/HomeWork/HWl2t5.java
@@ -0,0 +1,13 @@
+package HomeWork;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double b;
+        for (int a = 1; a <= 20; a++) {
+            b = a * 2.54;
+            System.out.println(b);
+        }
+
+    }
+}
