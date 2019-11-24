b5b29ecc16988ffb2679b00453313828c602caac
Richter Roman <57939572+axepoh616@users.noreply.github.com>
2019-11-24 19:32:29 +0300

    Update HWl2t4.java

--- src/HomeWork/HWl2t4.java
+++ src/HomeWork/HWl2t4.java
@@ -6,14 +6,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-       Scanner scanner = new Scanner(System.in);
-       int A =scanner.nextInt();
-       int B =scanner.nextInt();
-       int C = 0;
-       for (int D =0;D<B;D++) {
-           C=C+A;
-       }
-       System.out.println("A*B="+C);
-       //не работает при B<0
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int C = 0;
+        for (int D = 0; D < B; D++) {
+            C = C + A;
+        }
+        System.out.println("A*B=" + C);
+        //не работает при B<0
     }
 }
+
