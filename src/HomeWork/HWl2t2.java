d11e67e0065ca649499168bc08aefab2fbd5197b
axepoh616 <57939572+axepoh616@users.noreply.github.com>
2019-11-24 13:22:08 +0300

    Create HWl2t2.java

---
+++ src/HomeWork/HWl2t2.java
@@ -0,0 +1,15 @@
+package HomeWork;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 1;
+        for (int b = 3; b <= 24; b += 3) {
+            a *= 2;
+            System.out.println("across-" + b + "-hours will be-" + a + "-amoeba");
+        }
+
+    }
+
+
+}
