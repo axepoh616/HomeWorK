9da658859896a6f30d19a353d9153a2253e001e2
Richter Roman <57939572+axepoh616@users.noreply.github.com>
2019-11-28 22:22:32 +0300

    Create HWl1t6.java

---
+++ src/HomeWork/HWl1t6.java
@@ -0,0 +1,16 @@
+package HomeWork;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = -5;
+        int b = 4;
+        if (a > b) {
+            System.out.println(a);
+        } else if (a < b) {
+            System.out.println(b);
+        } else {
+            System.out.println("числа равны");
+        }
+    }
+}
