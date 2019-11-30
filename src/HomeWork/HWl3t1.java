a78e5b228306d299ec9d73014c769ec35d4f0169
Richter Roman <57939572+axepoh616@users.noreply.github.com>
2019-11-30 19:59:11 +0300

    Update HWl3t1.java

--- src/HomeWork/HWl3t1.java
+++ src/HomeWork/HWl3t1.java
@@ -4,42 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
         int[] numbers = new int[10];
-        numbers[0] = 2;
-        numbers[1] = 4;
-        numbers[2] = 6;
-        numbers[3] = 8;
-        numbers[4] = 10;
-        numbers[5] = 12;
-        numbers[6] = 14;
-        numbers[7] = 16;
-        numbers[8] = 18;
-        numbers[9] = 20;
-
-        for (int a = 1; a < numbers.length; a++) {
-            System.out.print(numbers[a] + " ");
-        }
-        System.out.println();
-        for (int b = 1; b < numbers.length; b++) {
-            System.out.println(numbers[b] + " ");
-        }
-
-    }
-}
-// или  
-package HomeWork;
-
-public class Main {
-
-    public static void main(String[] args) {
-        int[] numbers = {2,4,6,8,10,12,14,16,18,20};
         for (int a = 0; a < numbers.length; a++) {
+            numbers[a] = a + (a + 2);
             System.out.print(numbers[a] + " ");
         }
         System.out.println();
         for (int b = 0; b < numbers.length; b++) {
             System.out.println(numbers[b] + " ");
         }
-
     }
 }
-
